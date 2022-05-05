5b6c1b385d40404795405a982f50d613f955f4ea
7yIer <104940390+7yIer@users.noreply.github.com>
2022-05-05 11:40:50 -0700

    containsDuplicate 🍎🍊🍌🍇🍎🍍

---
+++ containsDuplicate.py
@@ -0,0 +1,5 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        # A set is unique.
+        # If the len of the set is not the same as nums, it must contain duplicates.
+        return len(set(nums)) != len(nums)
