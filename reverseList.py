bf79ece2b54d9fa890da16028592e4adfc938cfd
7yIer <104940390+7yIer@users.noreply.github.com>
2022-05-09 13:32:24 -0700

    ↱↲

---
+++ reverseList.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Traverse the linked list
+        # Hold on to the previous node
+        # Set the curr.next to the previous node
+        # Set the previous to curr
+        # set curr to the temp saved variable
+        
+        curr = head
+        prev = None
+        
+        while curr is not None:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
+        
